039e65f0474e662f704b7afeca819e0ff5bc176e
hubs0ne <hubsonemain@gmail.com>
2022-03-11 02:01:52 +0300

    add auth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css"
-// import { Auth } from "./components/auth"
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Auth } from "./components/auth"
 import { Header } from "./components/header"
 import { Journal } from "./components/journal"
 
 function App() {
+  const id = localStorage.getItem("id")
+  const [isAuth, setIsAuth] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    axios.get(`http://192.168.0.23:4000/api/trainer/check?id=${id}`)
+      .then(() => setIsAuth(true))
+      .finally(() => setLoading(false))
+  }, [id])
+
+  if (loading) return <h1>Загрузка..</h1>
+
   return (
     <div className="App">
       <Header />
       <div className="app-content">
-        {/* <Auth /> */}
-        <Journal />
+        {isAuth ? <Journal /> : <Auth setIsAuth={setIsAuth} />}
       </div>
     </div>
   )
--- src/components/auth/index.jsx
+++ src/components/auth/index.jsx
@@ -1,14 +1,28 @@
-import { useState } from "react"
-import { MyButton } from "../UI/button"
 import cn from "./style.module.css"
+import { useState } from "react"
+import axios from "axios"
+import { MyButton } from "../UI/button"
 
-export const Auth = () => {
+export const Auth = ({ setIsAuth }) => {
   const [login, setLogin] = useState("")
   const [password, setPassword] = useState("")
+
+  function log(e) {
+    e.preventDefault()
+    axios.post("http://192.168.0.23:4000/api/trainer/login", {
+      login, password
+    })
+      .then((res) => {
+        setIsAuth(true)
+        localStorage.setItem("id", res.data)
+      })
+      .catch((e) => console.log(e.message))
+  }
+
   return (
     <div className={cn.authWrapper}>
       <div>Пожалуйста войдите</div>
-      <form>
+      <form onSubmit={(e) => log(e)}>
         <div>
           <div>Введите логин</div>
           <input type="text" value={login} onChange={(e) => setLogin(e.target.value)} />
