cbba34c096c763ba5e6f0b76890a38d6632048b7
hubs0ne <hubsonemain@gmail.com>
2022-03-11 22:00:03 +0300

    complete list:

--- src/components/journal/list/index.jsx
+++ src/components/journal/list/index.jsx
@@ -2,6 +2,10 @@ import cn from "./style.module.css"
 import axios from "axios"
 import { api } from "../../../constants"
 import { useEffect, useState } from "react"
+import { MyButton } from "../../UI/button"
+import gold from "../../../assets/gold.svg"
+import silver from "../../../assets/silver.svg"
+import bronze from "../../../assets/bronze.svg"
 
 export const List = () => {
   const [spmans, setSpmans] = useState([])
@@ -16,8 +20,32 @@ export const List = () => {
     <div className={cn.listWrapper}>
       {spmans.map(spman =>
         <div key={spman._id} className={cn.spman}>
-          <div className={cn.name}>{spman.name}</div>
-          <div className={cn.age}>{spman.age}</div>
+          <div className={cn.infoMedals}>
+            <div className={cn.spmanInfo}>
+              <div className={cn.name}>{spman.name}</div>
+              <div className={cn.age}>Возраст: {spman.age}</div>
+            </div>
+            <div className={cn.medals}>
+              <div>
+                <img src={gold} alt="" />
+                <div>{spman.medals?.gold?.length}</div>
+              </div>
+              <div>
+                <img src={silver} alt="" />
+                <div>{spman.medals?.silver?.length}</div>
+              </div>
+              <div>
+                <img src={bronze} alt="" />
+                <div>{spman.medals?.bronze?.length}</div>
+              </div>
+            </div>
+          </div>
+          <div className={cn.rating}>
+            {spman.rating}
+          </div>
+          <div className={cn.addBtn}>
+            <MyButton>Медали</MyButton>
+          </div>
         </div>)}
     </div>
   )
